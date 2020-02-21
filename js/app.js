07b57d8ca49dde6e593d47bfa2a40a927f110c4d
Carlos Loureda <balanzeneto@gmail.com>
2020-02-21 14:43:50 +0100

    [feat]: Added js for updating width and margin of cards

--- js/app.js
+++ js/app.js
@@ -1,4 +1,34 @@
-(function() {
+/**
+ * My cards animations with SVG need some adjustment on small devices, so I
+ * added some JS for calculating with, top position of SVG and margin top of
+ * card content
+ */
+const cardResizeHandler = () => {
+  const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      entry = entry.target;
+      const cardWidth = entry.offsetWidth;
+
+      let top = -1 * Math.round(cardWidth / 1.74);
+      let marginTop = top - 20;
+      // console.log(`Top: ${top}, Margin Top: ${marginTop}`);
+
+      entry.querySelector(".card__svg").style.top = top;
+      entry.querySelector(".card__content").style.marginTop = `${marginTop}px`;
+    }
+  });
+
+  document
+    .querySelectorAll(".card")
+    .forEach(card => resizeObserver.observe(card));
+};
+
+/**
+ * On mobile devices I want to show the go to article link on the cards as it
+ * is written to work only with on hover or focus so I need to add this for
+ * scrolling on mobile devices.
+ */
+const activeCardOnScrollHandler = () => {
   const observaleSections = document.querySelectorAll(".card");
 
   const observer = new IntersectionObserver(
@@ -22,4 +52,7 @@
       observer.observe(section);
     });
   }
-})();
+};
+
+activeCardOnScrollHandler();
+cardResizeHandler();
